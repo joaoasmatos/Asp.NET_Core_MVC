e6cea7474467900e212400ec215f77a1a92895a5
joaoasmatos <joaoasmatos@gmail.com>
2021-09-11 14:26:48 -0300

    Removendo código antigo

--- Alura.ListaLeitura.App/Startup.cs
+++ Alura.ListaLeitura.App/Startup.cs
@@ -1,6 +1,4 @@
-﻿using Alura.ListaLeitura.App.Logica;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Routing;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Alura.ListaLeitura.App
@@ -17,18 +15,6 @@ namespace Alura.ListaLeitura.App
         public void Configure(IApplicationBuilder app)
         {
             app.UseMvcWithDefaultRoute();
-            //var routeBuilder = new RouteBuilder(app);
-            //routeBuilder.MapRoute("Livros/ParaLer", LivrosController.LivrosParaLer);
-            //routeBuilder.MapRoute("Livros/Lendo", LivrosController.LivrosLendo);
-            //routeBuilder.MapRoute("Livros/Lidos", LivrosController.LivrosLidos);
-            //routeBuilder.MapRoute("Livros/Detalhes/{id:int}", LivrosController.ExibeDetalhes);
-            //routeBuilder.MapRoute("Cadastro/NovoLivro", CadastroLogica.ExibeFormulario);
-            //routeBuilder.MapRoute("Cadastro/Incluir", CadastroLogica.ProcessaFormulario);
-            //routeBuilder.MapRoute("Cadastro/NovoLivro/{nome}/{autor}", CadastroLogica.NovoLivroParaLer);
-            
-            //var rotas = routeBuilder.Build();
-
-            //app.UseRouter(rotas);
         }
     }
 }
