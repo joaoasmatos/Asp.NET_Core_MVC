ddc023b0dfdd4421dbb208e4dbdd780117700b17
joaoasmatos <joaoasmatos@gmail.com>
2021-09-11 15:03:12 -0300

    Alterar formulario para padrão mvc

--- Alura.ListaLeitura.App/Logica/CadastroController.cs
+++ Alura.ListaLeitura.App/Logica/CadastroController.cs
@@ -2,6 +2,7 @@
 using Alura.ListaLeitura.App.Negocio;
 using Alura.ListaLeitura.App.Repositorio;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
@@ -23,10 +24,10 @@ namespace Alura.ListaLeitura.App.Logica
             return context.Response.WriteAsync("O livro foi adicionado com sucesso.");
         }
 
-        public static Task ExibeFormulario(HttpContext context)
+        public IActionResult ExibeFormulario()
         {
-            var html = HtmlUtils.CarregaArquivoHTML("formulario");
-            return context.Response.WriteAsync(html);
+            var html = new ViewResult { ViewName = "formulario" };
+            return html;
         }
 
         public string Incluir(Livro livro)
